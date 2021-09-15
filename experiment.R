d399519d9c42f1eac3d2bc51f1bd8cbc7ff81745
Daniel Amaral <silva.daniel86@Gmail.com>
2021-09-15 12:24:46 -0300

    fix models and add renv

--- experiment.R
+++ experiment.R
@@ -1,40 +1,61 @@
 # load data --------------------------------------------------------------------
-da_iris <- iris
+coln <- 24
+u <- glue::glue('https://archive.ics.uci.edu/ml/machine-learning-databases/00194/sensor_readings_{coln}.data')
+da_robot_colnames <- c(paste0('us', 1:coln), 'class')
+da_robot <- readr::read_csv(file = u, col_names = da_robot_colnames,
+                            col_types = paste0(c(rep('d', coln), 'c'), collapse = ""))
+
+# preprocessing ----------------------------------------------------------------
+da_robot <- da_robot |>
+  dplyr::mutate_if(rlang::is_double,
+                   function(x) (x - min(x))/(max(x) - min(x))) |>
+  dplyr::mutate(class = stringr::str_replace_all(class, '-', '_'),
+                class = stringr::str_to_lower(class),
+                class = factor(class, levels = c('move_forward', 'slight_right_turn',
+                                                 'sharp_right_turn', 'slight_left_turn')))
 
 # implementing models ----------------------------------------------------------
 ## Adaline
 ### adaline model with sigmoid activation
-adaline <- function(formula, data, epochs = 500, lr = 0.05) {
-  ## response in dummy format
-  truth_form <- glue::glue('~ -1 + {all.vars(formula)[1]}')
-  truth <- model.matrix(as.formula(truth_form), data = data)
-  colnames(truth) <- stringr::str_remove(colnames(truth), all.vars(formula)[1])
-
+adaline <- function(formula, data, epochs = 200, lr = 0.1) {
   # matrix of weights
-  ## ncol(truth) == n_class
-  W <- matrix(rnorm(ncol(truth) * ncol(data)), ncol = ncol(truth))
+  nclass <- nrow(dplyr::distinct(data[, all.vars(formula)[1]]))
+  W <- matrix(rnorm(nclass * ncol(data)), ncol = nclass)
 
   # double loop
   ## iterating first on rw (rows) and then on ep (epochs)
   for(ep in 1:epochs) {
+    ## shuffle data
+    data <- data[sample(1:nrow(data), nrow(data), replace = FALSE), ]
+    ## response in dummy format
+    truth_form <- glue::glue('~ -1 + {all.vars(formula)[1]}')
+    truth <- model.matrix(as.formula(truth_form), data = data)
+    colnames(truth) <- stringr::str_remove(colnames(truth), all.vars(formula)[1])
+    ## metrics vars
+    best_EQ <- 0 # for best epoch based on EQ
+    EQ <- 0 # store EQ for each epoch
     for(rw in 1:nrow(data)) {
       # data specification and prediction
       X <- model.matrix(formula, data = data[rw,]) # [1 Var1 Var2 ...] (1 x (p+1))
       Ui <- X %*% W # (1 x (p+1)) * ((p+1) x n_class) = (1 x n_class)
       Yi <- 1/(1 + exp(-Ui)) # sigmoid activation
-
       # error quantification
       Ei <- truth[rw,] - Yi
-
+      EQ <- EQ + 0.5*sum(Ei^2)
       # learning phase
       W <- W + lr*(t(X)/as.numeric(X%*%t(X)))%*%Ei
     }
+    # best epoch verification
+    if(EQ > best_EQ) {
+      best_epoch <- list(W = W)
+      best_eq <- EQ
+    }
+    #message(glue::glue('Epoch {ep}, MSE: {round(EQ/nrow(data), 5)}'))
   }
   # converting the function into a model like any other
   # already implemented in R
-  model <- structure(list(W = W, formula = formula,
+  model <- structure(list(W = best_epoch$W, formula = formula,
                           labels = colnames(truth)), class = "adaline")
-
   return(model)
 }
 ### logistic perceptron predict function
@@ -48,19 +69,24 @@ predict.adaline <- function(object, newdata) {
 
 ## PL
 ### logistic perceptron model
-perceptron_log <- function(formula, data, epochs = 500, lr = 0.05, mom = 0.01) {
-  ## response in dummy format
-  truth_form <- glue::glue('~ -1 + {all.vars(formula)[1]}')
-  truth <- model.matrix(as.formula(truth_form), data = data)
-  colnames(truth) <- stringr::str_remove(colnames(truth), all.vars(formula)[1])
-
+perceptron_log <- function(formula, data, epochs = 200, lr = 0.5, mom = 0.3) {
   # matrix of weights
-  W <- matrix(rnorm(ncol(truth) * ncol(data)), ncol = ncol(truth))
+  nclass <- nrow(dplyr::distinct(data[, all.vars(formula)[1]]))
+  W <- matrix(rnorm(nclass * ncol(data)), ncol = nclass)
   W_old <- W
 
   # double loop
   ## iterating first on rw (rows) and then on ep (epochs)
   for(ep in 1:epochs) {
+    ## shuffle data
+    data <- data[sample(1:nrow(data), nrow(data), replace = FALSE), ]
+    ## response in dummy format
+    truth_form <- glue::glue('~ -1 + {all.vars(formula)[1]}')
+    truth <- model.matrix(as.formula(truth_form), data = data)
+    colnames(truth) <- stringr::str_remove(colnames(truth), all.vars(formula)[1])
+    ## metrics vars
+    best_EQ <- 0 # for best epoch based on EQ
+    EQ <- 0 # store EQ for each epoch
     for(rw in 1:nrow(data)) {
       # data specification and prediction
       X <- model.matrix(formula, data = data[rw,]) # [1 Var1 Var2 ...] (1 x (p+1))
@@ -69,6 +95,9 @@ perceptron_log <- function(formula, data, epochs = 500, lr = 0.05, mom = 0.01) {
 
       # error quantification
       Ei <- truth[rw,] - Yi
+      EQ <- EQ + 0.5*sum(Ei^2)
+
+      # local gradients
       Di <- 0.5 * (1 - Yi^2) + 0.05
       DDi <- Ei * Di
 
@@ -77,6 +106,12 @@ perceptron_log <- function(formula, data, epochs = 500, lr = 0.05, mom = 0.01) {
       W <- W + lr*t(X)%*%DDi + mom*(W - W_old)
       W_old <- W_aux
     }
+    # best epoch verification
+    if(EQ > best_EQ) {
+      best_epoch <- list(W = W)
+      best_eq <- EQ
+    }
+    #message(glue::glue('Epoch {ep}, MSE: {round(EQ/nrow(data), 5)}'))
   }
   # converting the function into a model like any other
   # already implemented in R
@@ -125,38 +160,44 @@ predict.lmq <- function(object, newdata) {
 
 ## MLP
 ### multilayer perceptron model
-mlp <- function(formula, data, size = 10, epochs = 500, lr = 0.05, mom = 0.01) {
-  ## response in dummy format
-  truth_form <- glue::glue('~ -1 + {all.vars(formula)[1]}')
-  truth <- model.matrix(as.formula(truth_form), data = data)
-  colnames(truth) <- stringr::str_remove(colnames(truth), all.vars(formula)[1])
-
+mlp <- function(formula, data, size = 64, epochs = 300, lr = 0.2, mom = 0.4) {
   # matrix of weights
   ## input layer
   W <- matrix(rnorm(size * ncol(data)), ncol = size)
   W_old <- W
 
   ## hidden layer
-  H <- matrix(rnorm(ncol(truth) * (size + 1)), ncol = ncol(truth))
+  nclass <- nrow(dplyr::distinct(data[, all.vars(formula)[1]]))
+  H <- matrix(rnorm(nclass * (size + 1)), ncol = nclass)
   H_old <- H
 
   # double loop
   ## iterating first on rw (rows) and then on ep (epochs)
   for(ep in 1:epochs) {
+    ## shuffle data
+    data <- data[sample(1:nrow(data), nrow(data), replace = FALSE), ]
+    ## response in dummy format
+    truth_form <- glue::glue('~ -1 + {all.vars(formula)[1]}')
+    truth <- model.matrix(as.formula(truth_form), data = data)
+    colnames(truth) <- stringr::str_remove(colnames(truth), all.vars(formula)[1])
+    ## metrics vars
+    best_EQ <- 0 # for best epoch based on EQ
+    EQ <- 0 # store EQ for each epoch
     for(rw in 1:nrow(data)) {
       # data specification and prediction
       X <- model.matrix(formula, data = data[rw,]) # [1 Var1 Var2 ...] (1 x (p+1))
       ## hidden layer
       Ui <- X %*% W # (1 x (p+1)) * ((p+1) x size) = (1 x size)
       Zi <- 1/(1 + exp(-Ui)) # sigmoid activation
+      # Zi <- exp(Ui)/sum(exp(Ui))
       ## output layer
       Z <- cbind(1, Zi) # [1 Z1 Z2 ...] (1 x (size+1))
       Uk <- Z %*% H # (1 x (size+1)) * ((size+1) x n_class)
-      Yk <- 1./(1+exp(-Uk)) # sigmoid activation
-
+      Yk <- 1/(1+exp(-Uk)) # sigmoid activation
+      # Yk <- exp(Uk)/sum(exp(Uk))
       # error quantification
       Ek <- truth[rw,] - Yk
-
+      EQ <- EQ + 0.5*sum(Ek^2)
       # local gradients
       ## output layer
       Dk <- Yk * (1 - Yk) + 0.01
@@ -175,10 +216,16 @@ mlp <- function(formula, data, size = 10, epochs = 500, lr = 0.05, mom = 0.01) {
       W <- W + 2*lr*t(X)%*%DDi + mom*(W - W_old)
       W_old <- W_aux
     }
+    # best epoch verification
+    if(EQ > best_EQ) {
+      best_epoch <- list(W = W, H = H)
+      best_eq <- EQ
+    }
+    #message(glue::glue('Epoch {ep}, MSE: {round(EQ/nrow(data), 5)}'))
   }
   # converting the function into a model like any other
   # already implemented in R
-  model <- structure(list(W = W, H = H, formula = formula,
+  model <- structure(list(W = best_epoch$W, H = best_epoch$H, formula = formula,
                           labels = colnames(truth)), class = "mlp")
 
   return(model)
@@ -193,7 +240,7 @@ predict.mlp <- function(object, newdata) {
   ## output layer
   Z <- cbind(1, Zi) # [1 Z1 Z2 ...] (1 x (size+1))
   Uk <- Z %*% object$H # (1 x (size+1)) * ((size+1) x n_class)
-  Yk <- 1./(1+exp(-Uk)) # sigmoid activation
+  Yk <- 1/(1 + exp(-Uk)) # sigmoid activation
   estimate <- object$labels[max.col(Yk)] # get labels of the largest activation
   return(factor(estimate, levels = object$labels))
 }
@@ -217,30 +264,30 @@ get_metrics <- function(models, da_test, truth) {
 
 # run experiment ---------------------------------------------------------------
 ## settings for parallel processing (multiple iterations at the same time)
-globals <- list('da_iris' = da_iris, 'get_metrics' = get_metrics,
+globals <- list('da_robot' = da_robot, 'get_metrics' = get_metrics,
                 'adaline' = adaline, 'perceptron_log' = perceptron_log,
                 'lmq' = lmq, 'mlp' = mlp, 'predict.adaline' = predict.adaline,
                 'predict.pl' = predict.pl, 'predict.lmq' = predict.lmq,
                 'predict.mlp' = predict.mlp)
-future::plan(future::multisession, workers = 25) # 25 iterations at the same time
+future::plan(future::multisession, workers = 5) # 10 iterations at the same time
 ## loop 100x and assign results to `da_experiment`
 da_experiment <- furrr::future_map_dfr(1:100, function(seed) {
   ## data split 80/20 (train/test)
   set.seed(seed)
-  da_split <- rsample::initial_split(da_iris, prop = 0.8, strata = "Species")
+  da_split <- rsample::initial_split(da_robot, prop = 0.8, strata = "class")
   da_train <- rsample::training(da_split)
   da_test <- rsample::testing(da_split)
 
   ## apply models in train
-  mod_ada <- adaline(formula = Species ~ ., data = da_train)
-  mod_pl <- perceptron_log(formula = Species ~ ., data = da_train)
-  mod_lmq <- lmq(formula = Species ~ ., data = da_train)
-  mod_mlp <- mlp(formula = Species ~ ., data = da_train)
+  mod_ada <- adaline(formula = class ~ ., data = da_train)
+  mod_pl <- perceptron_log(formula = class ~ ., data = da_train)
+  mod_lmq <- lmq(formula = class ~ ., data = da_train)
+  mod_mlp <- mlp(formula = class ~ ., data = da_train)
 
   ## collect metrics in test
   metrics <- get_metrics(models = list('ada' = mod_ada, 'pl' = mod_pl,
                                        'lmq' = mod_lmq, 'mlp' = mod_mlp),
-                         da_test = da_test, truth = da_test[, "Species"])
+                         da_test = da_test, truth = da_test$class)
   return(metrics)
 }, .id = 'seed', .progress = TRUE,
 .options = furrr::furrr_options(seed = TRUE, globals = globals))
